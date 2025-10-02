b4a11ebbb30193dc45d99869270b55e61a1d810f
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2025-10-02 17:59:14 +0530

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,43 @@
+const { useState, useEffect, useRef } = React;
+
+export const OTPGenerator = () => {
+  const [otp, setOtp] = useState("Click 'Generate OTP' to get a code")
+  const [expire, setExpire] = useState("")
+  const btnRef = useRef(null)
+
+
+  const generateOTP = () => {
+    // random no 6 digits (100000, 999999)
+    
+    setOtp(Math.floor(Math.random() * 900000 + 100000))
+    setExpire("")
+    btnRef.current.disabled = true
+
+    let countDown = 5
+    setExpire(`Expire in: ${countDown} seconds`)
+    countDown--
+    const interval = setInterval(async () => {
+      if(countDown){
+         await setExpire(`Expire in: ${countDown} seconds`)
+        countDown-- 
+      }
+      else{
+        setExpire(`OTP expired. Click the button to generate a new OTP.`)
+        btnRef.current.disabled = false
+        clearInterval(interval)
+      }
+    }, 1000)   
+
+    return () => clearInterval(interval)
+
+  }
+  return(
+    <div className="container">
+      <h1 id="otp-title">OTP Generator</h1>
+      <h2 id="otp-display">{otp}</h2>
+      <p id="otp-timer" aria-live="polite">{expire}
+      </p>
+      <button ref={btnRef} id="generate-otp-button" onClick={generateOTP}>Generate OTP</button>
+    </div>
+  )
+};
